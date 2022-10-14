a0f65685f36f6f3550d33c0f1002fd23483f3430
sean.zhang <sean.zhang@digitwin.com>
2022-10-14 17:56:55 +0800

    install_kubectl.sh

---
+++ install_kubectl.sh
@@ -0,0 +1,6 @@
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+curl -LO https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256
+echo "$(<kubectl.sha256) kubectl" | sha256sum --check
+chmod +x kubectl
+mkdir -p ~/.local/bin/kubectl
+mv ./kubectl ~/.local/bin/kubectl

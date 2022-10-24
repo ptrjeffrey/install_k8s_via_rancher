beff29b99d7adc19303e102b66e4eaf126c344e9
sean.zhang <sean.zhang@digitwin.com>
2022-10-24 11:23:42 +0800

    bug fix

--- install_kubectl.sh
+++ install_kubectl.sh
@@ -2,5 +2,5 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s htt
 curl -LO https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256
 echo "$(<kubectl.sha256) kubectl" | sha256sum --check
 chmod +x kubectl
-mkdir -p ~/.local/bin/kubectl
-mv ./kubectl ~/.local/bin/kubectl
+#mkdir -p ~/.local/bin/kubectl
+mv ./kubectl /usr/local/bin/kubectl
